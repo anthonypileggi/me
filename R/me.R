2321792069225c55e6594b8ffba532307d13ea5d
anthonypileggi <anthony@ifixit.com>
2018-11-14 11:58:48 -0500

    update read_config to use 'me.toml' and use new 'skill' definitions

--- R/me.R
+++ R/me.R
@@ -1,7 +1,7 @@
 
 #' Read the config.toml file containing info about me
 #' @export
-read_config <- function(file = system.file("config.toml", package = "me")) {
+read_config <- function(file = system.file("me.toml", package = "me")) {
   configr::read.config(file)
 }
 
@@ -22,11 +22,5 @@ education <- function(x = read_config()) {
 
 #' @export
 skills <- function(x = read_config()) {
-  purrr::map_df(
-    names(x$skills),
-    function(skill) {
-      tmp <- purrr::map_df(x$skills[[skill]]$skill, tibble::as_tibble)
-      tmp$type <- skill
-      tmp
-    })
+  purrr::map_df(x$skill, tibble::as_tibble)
 }
